c58d3124b68a2c9206e3ede054746f76c5fd1d44
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-22 21:28:18 +0200

    feature: game: Balls of Steel

---
+++ src/components/Unity.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="display: grid; place-items: center">
+    <iframe
+      id="webgl_iframe"
+      frameborder="0"
+      allow="autoplay; fullscreen; vr"
+      allowfullscreen=""
+      allowvr=""
+      mozallowfullscreen="true"
+      src="https://64e50a20839a9019d51bb911--delicate-belekoy-4e9031.netlify.app/"
+      width="1200"
+      height="800"
+      onmousewheel=""
+      webkitallowfullscreen="true"
+    ></iframe>
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "unity",
+  // Vue component options
+};
+</script>
