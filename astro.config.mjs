fdd3b272b837787d33cc4ca8179a1c4d2b7b5309
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-25 13:32:41 +0200

    add: adapter for Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from "astro/config";
 
 import vue from "@astrojs/vue";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   //integrations: [tailwind(), vue()],
   integrations: [vue()],
+  output: "hybrid",
+  adapter: netlify(),
 });
