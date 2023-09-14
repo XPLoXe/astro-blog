8e0ca2e3f25d4005aa719afe16e4c334d50a7578
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-09-14 15:34:29 +0200

    First commit

---
+++ src/components/ImageCarousel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ssr-carousel :slides-per-page="2" :gutter="40" :peekGutter="false">
+      <slide :index="1"></slide>
+      <slide :index="2"></slide>
+      <slide :index="3"></slide>
+    </ssr-carousel>
+  </div>
+</template>
+
+<script setup>
+import SsrCarousel from "vue-ssr-carousel";
+import "vue-ssr-carousel/index.css";
+</script>
+
+<style lang="scss" scoped></style>
