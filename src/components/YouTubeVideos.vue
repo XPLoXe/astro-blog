b8084839b0c83d2e92a9c0fc94449dc0db8a98e6
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-09-16 12:59:06 +0200

    checkpoint: Encapsulate YoutubeProfile.astro videos into YouTubeVideos.vue

---
+++ src/components/YouTubeVideos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+//recieve an array of videos as a prop
+const props = defineProps({
+  videos: {
+    type: Array,
+    required: true,
+  },
+  channelInfo: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
