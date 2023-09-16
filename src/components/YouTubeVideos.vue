38cddb349a92da13be5fc09b80a40400ae2a505a
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-09-16 14:06:39 +0200

    feature: Video duration

--- src/components/YouTubeVideos.vue
+++ src/components/YouTubeVideos.vue
@@ -7,8 +7,8 @@
     :pauseAutoplayOnHover="true"
   >
     <Slide v-for="video in videos" :key="video.id">
-      <div class="w-full flex flex-col">
-        <div class="relative">
+      <div class="w-full h-auto flex flex-col items-center justify-center">
+        <div class="">
           <!-- Image Video -->
           <a
             :href="`https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}`"
@@ -19,7 +19,7 @@
           <p
             class="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py"
           >
-            1:15
+            {{ convertISO8601ToTime(video.contentDetails.duration) }}
           </p>
         </div>
 
@@ -27,16 +27,16 @@
           <!-- Profile Picture -->
           <a href="#">
             <img
-              src="https://picsum.photos/seed/1/40/40"
+              :src="channelInfo.snippet.thumbnails.default.url"
               class="rounded-full max-h-10 max-w-10"
             />
           </a>
 
           <!-- Description -->
-          <div class="flex flex-col">
+          <div class="flex flex-col items-center justify-center">
             <a href="#">
-              <p class="text-gray-100 text-sm font-semibold">
-                Learn CSS Box Model in 8 Minutes
+              <p class="text-purple-800 text-sm font-bold items-center">
+                {{ video.snippet.title }}
               </p>
             </a>
             <a class="text-gray-400 text-xs mt-2 hover:text-gray-100" href="#">
@@ -68,6 +68,34 @@ const props = defineProps({
     required: true,
   },
 });
+
+const convertISO8601ToTime = (duration) => {
+  {
+    const regex = /P(?:(\d+)D)?T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/;
+    const matches = duration.match(regex);
+
+    const days = parseInt(matches[1], 10) || 0;
+    const hours = days * 24 + (parseInt(matches[2], 10) || 0);
+    const minutes = parseInt(matches[3], 10) || 0;
+    const seconds = parseInt(matches[4], 10) || 0;
+
+    let result = [];
+    if (hours > 0) {
+      result.push(hours);
+    }
+
+    // Always show minutes, even if it's 0, but only if there are hours
+    if (hours > 0 || minutes > 0) {
+      result.push(String(minutes).padStart(2, "0"));
+    } else {
+      result.push("0");
+    }
+
+    result.push(String(seconds).padStart(2, "0"));
+
+    return result.join(":");
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
