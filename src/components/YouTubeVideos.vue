ca88a53b441f05c3e140237587d213767dad8b35
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-09-16 15:23:20 +0200

    feature: Hover, Duration and Publication date

--- src/components/YouTubeVideos.vue
+++ src/components/YouTubeVideos.vue
@@ -6,18 +6,28 @@
     :autoplay="2000"
     :pauseAutoplayOnHover="true"
   >
-    <Slide v-for="video in videos" :key="video.id">
-      <div class="w-full h-auto flex flex-col items-center justify-center">
+    <Slide
+      v-for="video in videos"
+      :key="video.id"
+      class="relative z-10 hover:z-20"
+    >
+      <div class="w-auto h-auto flex flex-col items-center justify-center">
         <div class="">
           <!-- Image Video -->
           <a
             :href="`https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}`"
+            target="_blank"
           >
-            <img :src="video.snippet.thumbnails.high.url" class="w-96 h-auto" />
+            <div class="relative group hover:z-10">
+              <img
+                :src="video.snippet.thumbnails.high.url"
+                class="w-full h-full transform transition-transform duration-300 group-hover:scale-150"
+              />
+            </div>
           </a>
 
           <p
-            class="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py"
+            class="absolute right-2 bottom-2 bg-purple-200 text-purple-700 text-xs px-1 py"
           >
             {{ convertISO8601ToTime(video.contentDetails.duration) }}
           </p>
@@ -25,24 +35,33 @@
 
         <div class="flex flex-row mt-2 gap-2">
           <!-- Profile Picture -->
-          <a href="#">
-            <img
-              :src="channelInfo.snippet.thumbnails.default.url"
-              class="rounded-full max-h-10 max-w-10"
-            />
-          </a>
+          <div class="row-span-2 col-span-2">
+            <a
+              href="https://www.youtube.com/channel/UCTlsYs4MWFD6b1yGoReg1cQ"
+              target="_blank"
+            >
+              <img
+                :src="channelInfo.snippet.thumbnails.default.url"
+                class="rounded-full h-10 w-10 absolute left-2 bottom-2"
+              />
+            </a>
+          </div>
 
           <!-- Description -->
           <div class="flex flex-col items-center justify-center">
-            <a href="#">
+            <a
+              :href="`https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}`"
+              target="_blank"
+            >
               <p class="text-purple-800 text-sm font-bold items-center">
                 {{ video.snippet.title }}
               </p>
             </a>
-            <a class="text-gray-400 text-xs mt-2 hover:text-gray-100" href="#">
-              Web Dev Simplified
-            </a>
-            <p class="text-gray-400 text-xs mt-1">241K views . 3 years ago</p>
+
+            <p class="text-gray-400 text-xs mt-1">
+              {{ video.statistics.viewCount }} Views -
+              {{ timeSince(video.snippet.publishedAt) }}
+            </p>
           </div>
         </div>
       </div>
@@ -56,7 +75,6 @@
 </template>
 
 <script setup>
-import { defineComponent } from "vue";
 import { Carousel, Navigation, Pagination, Slide } from "vue3-carousel";
 import "vue3-carousel/dist/carousel.css";
 
@@ -95,6 +113,44 @@ const convertISO8601ToTime = (duration) => {
 
     return result.join(":");
   }
+};
+
+const timeSince = (dateString) => {
+  const then = new Date(dateString);
+  const now = new Date();
+  const seconds = Math.floor((now - then) / 1000);
+  let interval = Math.floor(seconds / 31536000); // seconds in a year
+
+  if (interval >= 1) {
+    return interval + (interval === 1 ? " year ago" : " years ago");
+  }
+
+  interval = Math.floor(seconds / 2592000); // seconds in a month
+  if (interval >= 1) {
+    return interval + (interval === 1 ? " month ago" : " months ago");
+  }
+
+  interval = Math.floor(seconds / 604800); // seconds in a week
+  if (interval >= 1) {
+    return interval + (interval === 1 ? " week ago" : " weeks ago");
+  }
+
+  interval = Math.floor(seconds / 86400); // seconds in a day
+  if (interval >= 1) {
+    return interval + (interval === 1 ? " day ago" : " days ago");
+  }
+
+  interval = Math.floor(seconds / 3600); // seconds in an hour
+  if (interval >= 1) {
+    return interval + (interval === 1 ? " hour ago" : " hours ago");
+  }
+
+  interval = Math.floor(seconds / 60); // seconds in a minute
+  if (interval >= 1) {
+    return interval + (interval === 1 ? " minute ago" : " minutes ago");
+  }
+
+  return Math.floor(seconds) + " seconds ago";
 };
 </script>
 
